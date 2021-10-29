cc848d7e9ce9d7fd3f870c6a03695b4f591026d8
Vander <vanderson.barbosa2@fatec.sp.gov.br>
2021-10-28 23:02:53 -0300

    rotas finalizadas

--- routes/albuns.routes.js
+++ routes/albuns.routes.js
@@ -45,6 +45,53 @@ const albuns = [
     }
 ]
 
+
 router.get('/', (req,res) => {
     res.send(albuns);
-})
+});
+
+router.get("/:id", (req,res) =>{
+    const idParam = req.params.id;
+    const album = albuns.find(album => album.id == idParam); 
+    res.send(album);
+});
+
+router.post('/add',(req,res) =>{
+    const novoAlbum = req.body;
+    novoAlbum.id = albuns[albuns.length -1].id + 1;
+    albuns.push(novoAlbum);
+    res.send(albuns);
+});
+
+router.put('/edit/:id',(req,res) =>{
+    const idParam = req.params.id;
+    const novoAlbum = req.body;
+    let index = albuns.findIndex(album => album.id == idParam);
+    albuns[index] = {
+        ...albuns[index],
+        ...novoAlbum
+    }
+    res.send(albuns);
+});
+
+
+router.delete('/delete/:id',(req,res) =>{
+    const idParam = req.params.id;
+    const index = albuns.findIndex(album => album.id == idParam);
+    const nome = albuns[index];
+    albuns.splice(index, 1);
+    res.send({
+        message: `Album ${nome.titulo} excluido com sucesso !`,
+    })
+});
+
+router.put('/status/:id',(req,res) =>{
+    const idParam = req.params.id;
+    let index = albuns.findIndex(album => album.id == idParam);
+    if(albuns[index].assistido === false){
+        albuns[index].assistido = true
+    }else{
+        albuns[index].assistido = false
+    }
+    res.send(albuns);
+});
